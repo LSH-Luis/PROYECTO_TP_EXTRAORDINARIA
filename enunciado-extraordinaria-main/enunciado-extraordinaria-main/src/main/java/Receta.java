3ebe334d59871126959a57d7e2576789d7d765e7
luiss <luis.salinero.heras@alumno.upm.es>
2026-04-02 01:47:32 +0200

    Receta completa, falta ver si hay errores al correr test

--- enunciado-extraordinaria-main/enunciado-extraordinaria-main/src/main/java/Receta.java
+++ enunciado-extraordinaria-main/enunciado-extraordinaria-main/src/main/java/Receta.java
@@ -15,63 +15,144 @@ public class Receta {
     // @todo: atributos privados
 
     public Receta(String nombre, int maxIngredientes, int maxInstrucciones) {
-        // @todo
+        this.nombre= nombre;
+        this.maxIngredientes= maxIngredientes;
+        this.maxInstrucciones= maxInstrucciones;
+
+        this.ingrediente= ingrediente;
+        this.instruccion= instruccion;
+
+        this.numIngredientes= numIngredientes;
+        this.numInstrucciones= numInstrucciones;
     }
 
     public String getNombre() {
-        // @todo
+        return this.nombre;
     }
 
     public String[] getIngredientes() {
-        // @todo
+        String [] copiaIngredientes= new String[numIngredientes];
+        for( int i= 0; i< numIngredientes; i++){
+            copiaIngredientes[i]= ingrdientes[i];
+        }
+        return copiaIngredientes;
     }
 
     public String[] getInstrucciones() {
-        // @todo
+        String [ ] copiaInstrucciones= new String[numInstrucciones];
+        for ( int i = 0; i<maxInstrucciones; i++){
+            copiaInstrucciones[i]= instrucciones[i];
+        }
     }
 
     public int agregarIngrediente(String ingrediente) {
-        // @todo
+        if(ingrediente == null || ingrediente.trim().isEmpty()){
+            return ERROR_VALOR_INVALIDO;
+        }
+        if(ingredientesCompletos()){
+            return ERROR_INGREDIENTES_COMPLETOS;
+        }
+        ingredientes[numIngredientes]= ingrediente;
+        numIngredientes++;
+        return EXITO;
     }
 
     public int agregarInstruccion(String instruccion) {
-        // @todo
+        if(instruccion==null || instruccion.trim().isEmpty()){
+            return ERROR_VALOR_INVALIDO;
+        }
+        if(instruccionesCompletas()){
+            return ERROR_INSTRUCCIONES_COMPLETAS;
+        }
+
+        instrucciones[numInstrucciones]= instruccion;
+        numInstrucciones++;
+        return EXITO;
     }
 
     public boolean ingredientesCompletos() {
-        // @todo
+        if(numIngredientes()>=maxIngredientes){
+            return true;
+        }
+        else{
+            return false;
+        }
+
     }
 
     public boolean instruccionesCompletas() {
-        // @todo
+        if(numInstrucciones()>=maxInstrucciones){
+            return true;
+        }
+        else{
+            return true;
+        }
     }
 
     public int numIngredientes() {
-        // @todo
+        return numIngredientes;
     }
 
     public int numInstrucciones() {
-        // @todo
+        return numInstrucciones;
     }
 
     @Override
     public String toString() {
-        // @todo
+        StringBuilder sb= new StringBuilder();
+        sb.append("Receta: ").append(this.nombre).append("\n");
+        sb.append("Ingredientes: ").append("\n");
+        for(int i=0; i<numIngredientes;i++){
+            sb.apppend("- ").append(ingrediente[i]).append("\n");
+        }
+        sb.append("Instrucciones: ").append("\n");
+        for(int j=0; i<numInstrucciones;j++){
+            sb.apppend(j+1).append(instrucciones[j]).append("\n");
+        }
+        return sb.toString();
     }
 
     public String toRawString() {
-        // @todo
+        StringBuilder sb= new StringBuilder();
+        sb.append(this.nombre).append("\n");
+        for(int i=0; i<numIngredientes;i++){
+            sb.append(ingrediente[i]).append("\n");
+        }
+        sb.append("Instrucciones: ").append("\n");
+        for(int j=0; i<numInstrucciones;j++){
+            sb.append(instrucciones[j]).append("\n");
+        }
+        sb.append("-----").append("\n");
+        return sb.toString();
     }
 
     public int getMaxIngredientes() {
-        // @todo
+        return ingredientes.length;
     }
 
     public int getMaxInstrucciones() {
-        // @todo
+        return instrucciones.length;
     }
 
     public static Receta fromBufferedReader(BufferedReader reader, int maxIngredientes, int maxInstrucciones) throws IOException {
-        // @todo
+        String nombre= reader.readline();
+        if(nombre==null){
+            return null;
+        }
+
+        Receta nuevaReceta= new Receta(nombre, maxIngredientes, maxInstrucciones);
+
+        String linea= reader.readLine();
+        while(linea!= null && !linea.equals("INSTRUCCIONES")){
+            nuevaReceta.agregarIngrediente(linea);
+            linea=reader.readLine();
+        }
+
+        linea= reader.readLine();
+        while(linea!= null && !linea.equals("----")){
+            nuevaReceta.agregarInstruccion(linea);
+            linea= reader.readLine();
+        }
+        return nuevaReceta;
     }
 }
