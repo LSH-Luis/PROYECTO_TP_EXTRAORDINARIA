18f194017bbfbf50d47d6971f1acfb43b3753c3e
Daniel Aparisi <daniel.aparisi.lozano@gmail.com>
2026-04-02 09:36:59 +0200

    Utilidades echo (sin pasar test)

--- enunciado-extraordinaria-main/enunciado-extraordinaria-main/src/main/java/Utilidades.java
+++ enunciado-extraordinaria-main/enunciado-extraordinaria-main/src/main/java/Utilidades.java
@@ -5,22 +5,82 @@ import java.util.Scanner;
  */
 public class Utilidades {
     public static String leerCadena(Scanner teclado, String s) {
-        // @todo
+        System.out.print(s);
+        String cadena = teclado.nextLine();
+        return cadena;
     }
 
     public static int leerNumero(Scanner teclado, String mensaje, int minimo, int maximo) {
-        // @todo
+        boolean esValido = false;
+        int num = 0;
+        while (!esValido) {
+            System.out.print(mensaje);
+            if (teclado.hasNextInt()) { //¿es un entero?
+                num = teclado.nextInt();
+                if (num >= minimo && num <= maximo) esValido = true;//¿esta entreminimo y maximo?
+                else teclado.nextLine();
+            } else teclado.nextLine();
+        }
+        teclado.nextLine(); // Limpia el buffer de la entrada
+        return num;
+    }
+
+    // @todo
     }
 
     public static int leerDiaDeLaSemana(Scanner teclado, String mensaje) {
         // @todo
+            String dia;
+            int posicion;
+            do {
+                System.out.print(mensaje);
+                dia = teclado.nextLine().toUpperCase();
+                switch (dia) {
+                    case "L" -> posicion = 0;
+                    case "M" -> posicion = 1;
+                    case "X" -> posicion = 2;
+                    case "J" -> posicion = 3;
+                    case "V" -> posicion = 4;
+                    case "S" -> posicion = 5;
+                    case "D" -> posicion = 6;
+                    default -> posicion = -1;
+                }
+            } while (posicion == -1);
+            return posicion;
     }
 
     public static int diaSemanaAPosicion(String dia) {
+        int posicion;
+        String dia1 = dia.substring(0, 1).toUpperCase();//Extraemos Su incila en Mayuscula
+        switch (dia1) {
+            case "L" -> posicion = 0;
+            case "M" -> posicion = 1;
+            case "X" -> posicion = 2;
+            case "J" -> posicion = 3;
+            case "V" -> posicion = 4;
+            case "S" -> posicion = 5;
+            case "D" -> posicion = 6;
+            default -> posicion = -1;
+        }
+        return posicion;
+    }
+
         // @todo
     }
 
     public static String posicionADiaSemana(int pos) {
         // @todo
+        String dia = "Desconocido";
+        switch (pos) {
+            case 0 -> dia = "Lunes";
+            case 1 -> dia = "Martes";
+            case 2 -> dia = "Miércoles";
+            case 3 -> dia = "Jueves";
+            case 4 -> dia = "Viernes";
+            case 5 -> dia = "Sábado";
+            case 6 -> dia = "Domingo";
+        }
+        return dia; //si no se ha encontrado ----> desconocido
+    }
     }
 }
